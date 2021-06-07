ee2cdb554c75dc7c78a68cb5fa16f7292b5cbc1f
seigneurfuo <seigneurfuo@protonmail.com>
2021-06-07 23:23:48 +0200

    Séparation des mois et des années dans le fichier d'export TXT du planning.

--- Tools/docx_export.py
+++ Tools/docx_export.py
@@ -1,4 +1,5 @@
 # https://sysgenre.it/automatisation-de-template-docx/
+from datetime import datetime
 
 from database import *
 import docxtpl
@@ -26,8 +27,9 @@ def generate_planning(template_file, output_file):
     dates = Planning.select() \
         .join(Serie, on=(Planning.planning_fk_serie == Serie.serie_id)) \
         .join(Season, on=(Planning.planning_fk_season == Season.season_id)) \
-        .order_by(Planning.planning_date \
-        .desc(), Planning.planning_fk_serie.serie_sort_id, Planning.planning_fk_season.season_sort_id, Planning.planning_episode_id)
+        .order_by(Planning.planning_date.desc(),
+                  Planning.planning_fk_serie.serie_sort_id, Planning.planning_fk_season.season_sort_id,
+                  Planning.planning_episode_id.desc())
     
     line_break = docxtpl.RichText("\n")
     data = {
@@ -36,12 +38,35 @@ def generate_planning(template_file, output_file):
         "line_break": line_break
     }
 
+    # Variables pour les saut de lignes (permet de fait des groupes)
+    old_year = None
+    old_month = None
+
     output_file = output_file.replace(".docx", ".txt")
     with open(output_file, "w") as output_file_writer:
         for date in dates:
-            row = "{} - {} - {} - {}".format(date.planning_date, date.planning_fk_serie.serie_title, date.planning_fk_season.season_title, date.planning_episode_id)
+            # On ajoute des lignes spéciales
+            year = date.planning_date.strftime("%Y")
+            month = date.planning_date.strftime("%m")
 
+            if old_year and (year != old_year) :
+                msg = "\n" + "----- " * 5 + year +"----- " * 5 + "\n\n"
+                output_file_writer.write(msg)
+                print("\n", year)
+
+            elif old_month and (month != old_month):
+                output_file_writer.write("\n")
+
+                print("  ", month)
+
+            old_year = year
+            old_month = month
+
+            # On écrit la ligne normale
+            row = "{} - {} - {} - {}".format(date.planning_date, date.planning_fk_serie.serie_title, date.planning_fk_season.season_title, date.planning_episode_id)
             output_file_writer.write(row + "\n")
+
+
 
     msg = "Nombre de lignes lignes exportées: {}.".format(len(dates))
     print(msg)
